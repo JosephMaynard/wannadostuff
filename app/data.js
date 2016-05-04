446274332510d39b2bb02099b574d607c651d6fe
Joseph Maynard <joseph.maynard@gmail.com>
2016-05-04 21:14:46 +1000

    Initial commit

---
+++ app/data.js
@@ -0,0 +1,37 @@
+// Choose activity
+
+var activities ={
+	bar_games:{
+		name:"Bar Games",
+		subcategories:{
+			pool: {
+				name: "Pool",
+				id: 1,
+				icon: 'icon'
+			},
+			buckHunter:{
+				name: "Buck Hunter"
+				id: 2,
+				icon: 'icon'
+			},
+			darts:{
+				name: "Darts"
+				id: 2,
+				icon: 'icon'
+			}
+		}
+	},
+	nightLife:{
+		name:"Night Life",
+		subcategories:{
+			clubbing:"Clubbing",
+				id: 1,
+				icon: 'icon'
+			},
+			wingMan:{
+
+			}
+		}
+		
+	}
+}
